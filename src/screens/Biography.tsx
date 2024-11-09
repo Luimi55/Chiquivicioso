ea082382b7e4d94bdea00b76de329d2760799800
Luis Rosa <miguelrosa0105@gmail.com>
2024-11-08 20:10:58 -0400

    Chore: Change biography image

--- src/screens/Biography.tsx
+++ src/screens/Biography.tsx
@@ -7,6 +7,7 @@ import Image from "react-bootstrap/Image";
 import image1 from "/img/Vicioso-BW-600x600.png";
 import image2 from "/img/CHIQUI JOVEN.png"
 import image3 from "/img/Flor dibujo.png"
+import image4 from "/img/CHIQUI-JOVEN 2.jpg"
 
 type Props = {};
 
@@ -18,7 +19,7 @@ const Biography = (props: Props) => {
         <Row>
           <Col xs={12} sm={6}>
             <Image
-              src="https://elmunicipe.com/wp-content/uploads/2023/03/web_-_cultura_litrratura_chiqui_vicioso.jpg"
+              src={image4}
               // width={620}
               // height={370}
               fluid
@@ -32,15 +33,16 @@ cursó sus estudios en Estados Unidos y Brasil. Licenciada en Sociología e Hist
 Nueva York, Maestría en Educación por la Universidad de Columbia y Postgrado en Administración Cultural por la Fundación Getúlio 
 Vargas (Brasil). 
             </p>
-            <p>
-            Ha sido reconocida con el galardón Anacaona de Oro en Literatura y la Medalla de Oro al Mérito a la Mujer en 1992.
-Con obras destacadas como <b>Volver a vivir: ensayos sobre Nicaragua, Wish-ky Sour, Salomé Ureña de Henríquez (1859-1897): a cien años
- de su magisterio</b>.
-            </p>
+
           </Col>
         </Row>
         <Row className="mt-4">
           <Col xs={12}>
+          <p>
+            Ha sido reconocida con el galardón Anacaona de Oro en Literatura y la Medalla de Oro al Mérito a la Mujer en 1992.
+Con obras destacadas como <b>Volver a vivir: ensayos sobre Nicaragua, Wish-ky Sour, Salomé Ureña de Henríquez (1859-1897): a cien años
+ de su magisterio</b>.
+            </p>
           <p>Hija de <b>Juan Antonio Vicioso Contín y María Luisa Sánchez</b>. Licenciada en Sociología e Historia de América Latina por The City University of New York (Brooklyn College). Hizo una maestría en 
 Diseño de Programas Educativos en la Universidad de Columbia y estudió Administración de Proyectos Culturales en la Fundación 
 Getúlio Vargas, de Río de Janeiro, Brasil.</p>
@@ -66,7 +68,9 @@ Fue <b>Directora de Educación de Pro Familia (1981-1985), Consultora del Progra
           <p>
           Al inicio de la década de los ochenta fundó el <b>Círculo de Mujeres Poetas</b>. En 1988 la Sociedad Dominicana de Escritores le concedió
  el prestigioso Caonabo de Oro y posteriormente, en 1992, la <b>Dirección General de Promoción de la Mujer le entregó la Medalla de Oro
- al Mérito a la Mujer más Destacada del Año</b>.
+ al Mérito a la Mujer más Destacada del Año</b>. El 21 de abril de 2004 ganó <b>La Orden de Don Quijote, el más alto honor conferido por Sigma Delta Pi,</b>
+  reconoce el servicio excepcional y meritorio en los campos de la erudición hispánica, la enseñanza del español y la promoción de las buenas relaciones
+  entre los países de habla inglesa y los de habla española.
           </p>
 
           <p>
@@ -79,7 +83,7 @@ Fue <b>Directora de Educación de Pro Familia (1981-1985), Consultora del Progra
 
           </Col>
         </Row>
-        <Image src={image3} className="biographyOverlayImage"/>
+        {/* <Image src={image3} className="biographyOverlayImage"/> */}
       </Container>
     </Container>
   );
